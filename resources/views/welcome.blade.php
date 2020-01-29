24c0025a52cbaff2888a2a06673678858b420651
Tom Coward <tomcoward02@icloud.com>
2020-01-29 17:28:09 +0000

    Update Welcome page, and change URL to redirect to for 'guest' middleware.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,19 +65,10 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class="top-right links">
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            </div>
 
             <div class="content">
                 <div class="title m-b-md">
@@ -85,7 +76,7 @@
                 </div>
 
                 <div class="links">
-                    <p>Coming soon. By Tom Coward.</p>
+                    <p>Get involved. <a href="{{ route('register') }}">Join now</a>.</p>
                 </div>
             </div>
         </div>
