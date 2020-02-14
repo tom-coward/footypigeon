610aba1be2ec76588930718b723074b29170efa5
Tom Coward <tomcoward02@icloud.com>
2020-02-14 22:13:55 +0000

    Created Leagues system routes and `CheckIfLeagueAdmin` middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'leagueAdmin' => \App\Http\Middleware\CheckIfLeagueAdmin::class,
     ];
 
     /**
