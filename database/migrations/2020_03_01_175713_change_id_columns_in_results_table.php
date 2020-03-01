538bcba3c898c8fcd44b1534bf73447db21ca833
Tom Coward <tomcoward02@icloud.com>
2020-03-01 20:43:52 +0000

    Reverted ID changes to 'predictions' DB table (as IDs wouldn't be unique due to there being several records for the same fixture).

--- database/migrations/2020_03_01_175713_change_id_columns_in_results_table.php
+++ database/migrations/2020_03_01_175713_change_id_columns_in_results_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeIdColumnsInPredictionsAndResultsTables extends Migration
+class ChangeIdColumnsInResultsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,6 @@ class ChangeIdColumnsInPredictionsAndResultsTables extends Migration
      */
     public function up()
     {
-        Schema::table('predictions', function (Blueprint $table) {
-            $table->string('id')->change();
-        });
-
         Schema::table('results', function (Blueprint $table) {
             $table->string('id')->change();
         });
@@ -29,10 +25,6 @@ class ChangeIdColumnsInPredictionsAndResultsTables extends Migration
      */
     public function down()
     {
-        Schema::table('predictions', function (Blueprint $table) {
-            $table->bigIncrements('id')->change();
-        });
-
         Schema::table('results', function (Blueprint $table) {
             $table->bigIncrements('id')->change();
         });
