ea2916b60e795557c7c2331d144ac341b39b9246
Tom Coward <tom@versobit.com>
2021-02-07 17:38:36 +0000

    Added message to 'My Results' page if no results yet recorded

--- resources/views/my-results/index.blade.php
+++ resources/views/my-results/index.blade.php
@@ -7,35 +7,39 @@
                 <h1>My Results</h1>
                 <hr />
 
-                <!-- TODO: center align pagination links -->
-                {{ $predictions->links() }}
-
-                <br />
-
-                @foreach($predictions as $prediction)
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <h4>{{ $prediction->home_team_name }} vs. {{ $prediction->away_team_name }}</h4>
-
-                            <!-- User's Prediction -->
-                            <h5>Your Prediction</h5>
-                            <h2 {{ $prediction->points_awarded > 0 ? 'class=text-success' : ''}}>{{ $prediction->home_team_goals }} - {{ $prediction->away_team_goals }}</h2>
-
-                            <!-- Final Result -->
-                            <h5>Final Result</h5>
-                            <h2>{{ $prediction->result->home_team_goals }} - {{ $prediction->result->away_team_goals }}</h2>
-
-                            @isset($prediction->points_awarded)
-                                <p class="text-center">You were awarded <b>{{ $prediction->points_awarded }} points</b> for this match.</p>
-                            @endisset
-                        </div>
-                    </div>
+                @if(count($predictions) > 0)
+                    <!-- TODO: center align pagination links -->
+                    {{ $predictions->links() }}
 
                     <br />
-                @endforeach
 
-                <!-- TODO: center align pagination links -->
-                {{ $predictions->links() }}
+                    @foreach($predictions as $prediction)
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <h4>{{ $prediction->home_team_name }} vs. {{ $prediction->away_team_name }}</h4>
+
+                                <!-- User's Prediction -->
+                                <h5>Your Prediction</h5>
+                                <h2 {{ $prediction->points_awarded > 0 ? 'class=text-success' : ''}}>{{ $prediction->home_team_goals }} - {{ $prediction->away_team_goals }}</h2>
+
+                                <!-- Final Result -->
+                                <h5>Final Result</h5>
+                                <h2>{{ $prediction->result->home_team_goals }} - {{ $prediction->result->away_team_goals }}</h2>
+
+                                @isset($prediction->points_awarded)
+                                    <p class="text-center">You were awarded <b>{{ $prediction->points_awarded }} points</b> for this match.</p>
+                                @endisset
+                            </div>
+                        </div>
+
+                        <br />
+                    @endforeach
+
+                    <!-- TODO: center align pagination links -->
+                    {{ $predictions->links() }}
+                @else
+                    <p>You do not yet have any recorded results. Please check back later.</p>
+                @endif
             </div>
         </div>
     </div>
