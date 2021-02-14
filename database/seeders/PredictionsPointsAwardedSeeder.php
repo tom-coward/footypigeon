5727e06eea6fcf2c40c78f7d7e297fd876149655
Tom Coward <tom@versobit.com>
2021-02-14 13:26:11 +0000

    Fix issues preventing PredictionsPointsAwardedSeeder from running

--- database/seeders/PredictionsPointsAwardedSeeder.php
+++ database/seeders/PredictionsPointsAwardedSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 
 use App\Prediction;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+use Carbon\Carbon;
 
 class PredictionsPointsAwardedSeeder extends Seeder
 {
@@ -16,32 +18,37 @@ class PredictionsPointsAwardedSeeder extends Seeder
      */
     public function run()
     {
-        foreach(Prediction::whereNotNull('points_awarded')->get() as $prediction){
-            $predictionHomeGoals = $prediction->home_team_goals;
-            $predictionAwayGoals = $prediction->away_team_goals;
-            $resultHomeGoals = $prediction->result->home_team_goals;
-            $resultAwayGoals = $prediction->result->away_team_goals;
+        // Collect latest results
+        Artisan::call('results:get');
 
-            $predictionHomeWin = $predictionHomeGoals > $predictionAwayGoals;
-            $resultHomeWin = $resultHomeGoals > $resultAwayGoals;
-            $predictionAwayWin = $predictionAwayGoals > $predictionHomeGoals;
-            $resultAwayWin = $resultAwayGoals > $resultHomeGoals;
-            $predictionDraw = $predictionHomeGoals == $predictionAwayGoals;
-            $resultDraw = $resultHomeGoals == $resultAwayGoals;
+        foreach(Prediction::whereNull('points_awarded')->get() as $prediction){
+            if($prediction->result){
+                $predictionHomeGoals = $prediction->home_team_goals;
+                $predictionAwayGoals = $prediction->away_team_goals;
+                $resultHomeGoals = $prediction->result->home_team_goals;
+                $resultAwayGoals = $prediction->result->away_team_goals;
 
-            // Correct score (20pts)
-            if($predictionHomeGoals == $resultHomeGoals AND $predictionAwayGoals == $resultAwayGoals){
-                DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 20]);
-            }
-            // Correct winner (10pts)
-            elseif(($predictionHomeWin == true AND $resultHomeWin == true)
-                OR ($predictionAwayWin == true AND $resultAwayWin == true)
-                OR ($predictionDraw == true AND $resultDraw == true)){
-                DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 10]);
-            }
-            // No points
-            else{
-                DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 0]);
+                $predictionHomeWin = $predictionHomeGoals > $predictionAwayGoals;
+                $resultHomeWin = $resultHomeGoals > $resultAwayGoals;
+                $predictionAwayWin = $predictionAwayGoals > $predictionHomeGoals;
+                $resultAwayWin = $resultAwayGoals > $resultHomeGoals;
+                $predictionDraw = $predictionHomeGoals == $predictionAwayGoals;
+                $resultDraw = $resultHomeGoals == $resultAwayGoals;
+
+                // Correct score (20pts)
+                if($predictionHomeGoals == $resultHomeGoals AND $predictionAwayGoals == $resultAwayGoals){
+                    DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 20]);
+                }
+                // Correct winner (10pts)
+                elseif(($predictionHomeWin == true AND $resultHomeWin == true)
+                    OR ($predictionAwayWin == true AND $resultAwayWin == true)
+                    OR ($predictionDraw == true AND $resultDraw == true)){
+                    DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 10]);
+                }
+                // No points
+                else{
+                    DB::table('predictions')->where('id', $prediction->id)->update(['points_awarded' => 0]);
+                }
             }
         }
     }
